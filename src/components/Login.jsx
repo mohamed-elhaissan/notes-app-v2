316fbb849607fa5fedd501489b634ac1205c6afd
mohamed-elhaissan <mhalwasoffice@gmail.com>
2024-11-24 09:00:49 +0100

    updating

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,27 +1,34 @@
+import { useState } from "react";
+import { motion } from "framer-motion";
+
 export default function Login() {
+  const [cin, setCin] = useState("");
+  const [password, setPassword] = useState("");
+  const handleFormSubmit = (e)=>{
+    e.preventDefault();
+    if (cin.length == 0 || password.length ==0) {
+      console.error('something wrong');
+      
+    }
+  }
   return (
     <div className="h-[100vh] flex  items-center justify-center">
       <div className="bg-white w-1/3 shadow-md p-4 rounded-lg">
         <h1 className="text-center text-3xl">Login</h1>
         <p className="text-center opacity-50 mt-2 mb-5">To get Started</p>
-        <form
-          className="flex flex-col"
-          method="POST"
-          
-        >
+        <form className="flex flex-col" method="POST" onSubmit={handleFormSubmit}>
           <div className="flex flex-col">
             <label>cin</label>
-            <input
-              className="border py-2 pl-2 outline-[#A1A1AA] mb-2"
+            <input onChange={(event)=>{setCin(event.target.value)}}
+              className="border py-2  pl-2 outline-[#A1A1AA] mb-2"
               name="email"
-              type="email"
-              placeholder="m@exemple.com"
-              required
+              type="text"
+              
             />
           </div>
           <div className="flex flex-col">
             <label>password</label>
-            <input
+            <input onChange={(event)=>setPassword(event.target.password)}
               className="border py-2 pl-2 outline-[#A1A1AA] mb-2"
               name="password"
               type="password"
@@ -37,4 +44,13 @@ export default function Login() {
       </div>
     </div>
   );
+}
+
+
+const NotficationError = ()=>{
+  return (
+    <div>
+      <p>New notification from John Anderson</p>
+    </div>
+  )
 }
