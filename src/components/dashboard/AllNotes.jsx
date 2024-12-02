27e5641f7369c7f911e336f2b5e5de8c9642c790
mohamed-elhaissan <mhalwasoffice@gmail.com>
2024-12-02 20:01:30 +0100

    updating

--- src/components/dashboard/AllNotes.jsx
+++ src/components/dashboard/AllNotes.jsx
@@ -1,20 +1,35 @@
-
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { request } from "../../utils/axiosUtilis";
 
-export default function AllNotes(){
-    const fetchAllNotes = async ()=>{
-        await request({url : '/notes'}).then((reponse)=>{
-            console.log(reponse);
+export default function AllNotes() {
+    const [notes,setNotes] = useState([])
+  const fetchAllNotes = async () => {
+    await request({ url: "/notes" })
+      .then((reponse) => {
+            setNotes(...reponse.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  useEffect(() => {
+    fetchAllNotes();
+  }, []);
+  return (
+    <>
+      <div>
+        <table>
+            <thead>
+                <tr>
+                    <th>Note</th>
+                    <th>Note</th>
+                </tr>
+            </thead>
+        </table>
+        {notes.map((item,index)=>(
             
-        }).catch((err)=>{console.log(err)})
-    }
-    useEffect(()=>{
-        fetchAllNotes()
-    },[])
-    return (
-        <div>
-            
-        </div>
-    )
+        ))}
+      </div>
+    </>
+  );
 }
