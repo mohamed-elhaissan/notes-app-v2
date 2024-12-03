e92ef766a1ad100f67c7a7477c6087786e8ce5ce
mohamed-elhaissan <mhalwasoffice@gmail.com>
2024-12-03 15:52:42 +0100

    updating

--- src/components/dashboard/AdddNotes.jsx
+++ src/components/dashboard/AdddNotes.jsx
@@ -1,19 +1,36 @@
 import { motion } from "motion/react";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { request } from "../../utils/axiosUtilis";
 import { FiPlus } from "react-icons/fi";
-
-import Select from "react-select/base";
+import makeAnimated from "react-select/animated";
+import Select from "react-select";
 const AddNote = () => {
   const [showAddNote, setShowAddNote] = useState(true);
   const [options, setOptions] = useState([]);
+  const titleInputRef = useRef();
+  const contentInputRef = useRef();
+
   const getAllUser = async () => {
     await request({
       url: "/users",
     }).then(({ data }) => {
-      console.log(data);
-      setOptions([...data]);
+      data.forEach((item) => {
+        setOptions((prev) => [
+          ...prev,
+          {
+            value: item.id,
+            label: `${item.first_name.toLowerCase()} ${item.last_name.toLowerCase()}`,
+          },
+        ]);
+      });
     });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (condition) {
+      
+    }
   };
   useEffect(() => {
     getAllUser();
@@ -21,7 +38,6 @@ const AddNote = () => {
 
   return (
     <>
-      <div className="fixed opacity-50 left-0 top-0 w-full h-full bg-black z-10"></div>
       <motion.button
         onClick={() => setShowAddNote(!showAddNote)}
         whileTap={{ scale: 0.9 }}
@@ -32,18 +48,47 @@ const AddNote = () => {
         New Notes
       </motion.button>
       {showAddNote && (
-        <motion.div
-        className="bg-red-950 absolute p-4 w-96"
-      initial={{ opacity: 0, x: "-50%", y: "-50%" }}
-      animate={{ opacity: 1, x: "50%", y: "50%" }}
-      transition={{ duration: 0.3 }}
-        >
-          <form>
-            <input type="text" placeholder="note Title.." />
-            <textarea rows={2} placeholder="contnent here "></textarea>
+        <motion.div className="fixed z-20 flex items-center justify-center bg-slate-900/20 backdrop-blur w-full h-full left-0 top-0 ">
+          <form
+            className="bg-white flex flex-col p-4 w-[30%] rounded"
+            onSubmit={handleSubmit}
+          >
+            <label htmlFor="title" className="font-semibold tracking-tighter">
+              Title
+            </label>
+            <input
+            ref={}
+              type="text"
+              placeholder="note Title.."
+              className="outline-none border-2 px-2 py-2 rounded mb-2 text-sm "
+            />
+            <label htmlFor="title" className="font-semibold tracking-tighter">
+              Content
+            </label>
+            <textarea
+              ref={contentInputRef}
+              rows={2}
+              placeholder="contnent here "
+              className="outline-none border-2 px-2 py-2 rounded mb-2 text-sm "
+            ></textarea>
             <div>
-              <Select />
+              <p className="font-semibold">Shared With</p>
+              <Select
+                options={options}
+                closeMenuOnSelect={false}
+                components={makeAnimated()}
+                isMulti
+              />
             </div>
+            <button className="bg-black text-white text-sm rounded py-3 px-2 mt-2">
+              Add Note
+            </button>
+            <button
+              className="border-2 text-sm rounded py-2 px-2 mt-2"
+              onClick={() => setShowAddNote(false)}
+            >
+              Cancel
+            </button>
           </form>
         </motion.div>
       )}
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
@@ -7,7 +7,7 @@ import LoginProvider from "./context/UserContext.jsx";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  
     <BrowserRouter>
       <LoadingProvider>
         <LoginProvider>
@@ -15,5 +15,5 @@ createRoot(document.getElementById("root")).render(
         </LoginProvider>
       </LoadingProvider>
     </BrowserRouter>
-  </StrictMode>
+  
 );
